d3b05cb655c34fc4a709f520856c1361c963534d
Russell Henry <rjhenry1014@gmail.com>
2016-12-07 19:16:58 +0000

    Added add, post and delete endpoints

--- server.js
+++ server.js
@@ -2,8 +2,77 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var config = require('./config');
+
 var app = express();
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+var Weapon = require('models/weapons');
+
+app.get('/', function(request, response) {
+    Weapon.find(function(err, weapons) {
+        if (err) {
+            return response.status(500).json({
+                message: 'A server error occured'
+            });
+        }
+        response.json(weapons);
+    });
+});
+
+app.post('/weapons/:name', function(request, response) {
+    Weapon.create({
+        name: request.body.name
+    }, function(err, weapon) {
+        if (err) {
+            return response.status(500).json({
+                message: 'A server error occured'
+            });
+        }
+    });
+});
+
+app.delete('/weapons/:name', function(request, response) {
+    Weapon.remove({
+        name: request.params.name
+    }, function(err, weapon) {
+        if (err) {
+            response.status(500).json({
+                message: 'A server error occured'
+            });
+        }
+    });
+});
+
+app.use('*', function(request, response) {
+    response.status(404).json({
+        message: 'Not Found'
+    });
+});
 
 app.listen(process.env.PORT || 8080);
