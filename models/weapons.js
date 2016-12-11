89cd9ac6d25179c24e4a6098e01de0d924d62b21
Russell Henry <rjhenry1014@gmail.com>
2016-12-11 03:28:06 +0000

    Removed franchise reference

--- models/weapons.js
+++ models/weapons.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 
 var weaponSchema = new mongoose.Schema({
        name: {type: String, unique: true},
-       franchise: {type: mongoose.Schema.Types.ObjectId, ref: "Franchise"},
        cost: Number,
        strength: Number
     });
