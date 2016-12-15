892e8f1092bead9b1ffafb00a7e6b07d5f8bbc82
Russell Henry <rjhenry1014@gmail.com>
2016-12-15 21:08:53 +0000

    Added authentication for mLab mongo access

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+var dbuser = process.env.dbuser;
+var dbpass = process.env.dbpass;
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/weapons' :
+                            'mongodb://' + dbuser + ':' + dbpass + '@ds133398.mlab.com:33398/capstone-weapon-database' :
                             'mongodb://localhost/weapons-dev');
 exports.PORT = process.env.PORT || 8080;
