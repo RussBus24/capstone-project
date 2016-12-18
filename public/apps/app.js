e36c09e8dd1866290dda2b20f2e27bca22902e5c
Russell Henry <rjhenry1014@gmail.com>
2016-12-18 19:04:07 +0000

    Added Franchise fields and JS code

--- public/apps/app.js
+++ public/apps/app.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 	$('#addWeapon').submit(function(event) {
 		event.preventDefault();
-		var addName = $('#name').val();
-		var addCost = $('#cost').val();
+		var addName = $('#addWeaponName').val();
+		var addCost = $('#addWeaponCost').val();
 		var addStrength = $('#strength').val();
 		console.log(addName);
-		sendQuery(addName, addCost, addStrength);
+		sendWeaponQuery(addName, addCost, addStrength);
+	});
+	$('#addFranchise').submit(function(event) {
+		event.preventDefault();
+		var addName = $('#addFranchiseName').val();
+		var addPublisher = $('#addPublisherName').val();
+		console.log(addName);
+		sendFranchiseQuery(addName, addPublisher);
 	});
 });
 
-function sendQuery(addName, addCost, addStrength) {
+function sendWeaponQuery(addName, addCost, addStrength) {
 	
 	var queryData = {
 		name: addName,
@@ -23,6 +30,29 @@ function sendQuery(addName, addCost, addStrength) {
 		type: "POST",
 		contentType: 'application/json'
 	})	
+	
+	.done(function(data) {
+		console.log(data);
+	})
+	
+	.fail(function(jqXHR, error) {
+		console.log(error);
+	});
+}
+
+function sendFranchiseQuery(addName, addPublisher) {
+	
+	var queryData = {
+		name: addName,
+		publisher: addPublisher
+	};
+	
+	$.ajax({
+		url: "https://vast-depths-38075.herokuapp.com/franchise",
+		data: JSON.stringify(queryData),
+		type: "POST",
+		contentType: 'application/json'
+	})
 	
 	.done(function(data) {
 		console.log(data);
